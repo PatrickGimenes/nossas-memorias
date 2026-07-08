d450500576df8b3e25de966ce9b1eec7a3129447
Patrick Gimenes <p.gimenes@outlook.com>
2026-07-08 20:49:40 -0300

    Adicionado novos encontros

--- js/data.js
+++ js/data.js
@@ -13,9 +13,33 @@ const memories = [
     description: "Quanto pesa essa cadeira?",
   },
   {
-    title: "Terceiro encontro - Caldas",
-    date: "2026-07-04",  
+    title: "O primeiro mimo",
+    date: "2026-07-08",  
     image: "assets/photos/date03.jpeg",  
+    description: "O primeiro de muitos.",
+  },
+  {
+    title: "Terceiro encontro - Pizza na roça",
+    date: "2026-07-11",  
+    image: "assets/photos/date04.webp",  
+    description: "É tão boa quanto a italiana?",
+  },
+  {
+    title: "Quarto encontro - Caldas",
+    date: "2026-07-18",  
+    image: "assets/photos/date05.jpeg",  
     description: "Eu já falei que gosto de ir para Caldas?",
+  },
+  {
+    title: "Quinto encontro - Holambra",
+    date: "2026-07-25",  
+    image: "assets/photos/date06.jpg",  
+    description: "Uma flor para uma flor!",
+  },
+  {
+    title: "Sexto encontro - Serra Negra",
+    date: "2026-07-18",  
+    image: "assets/photos/date07.jpg",  
+    description: "Parece a Itália, então deve ter pizza italiana.",
   },
 ];
